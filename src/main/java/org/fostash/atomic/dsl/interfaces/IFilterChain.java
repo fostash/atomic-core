909c59527f0915d89c17128ef7e54c8b12f55c88
fost <fausto.bonacina@gmail.com>
2018-07-09 16:33:02 +0200

    changed IFilterChain add IGroupable for groupBy

--- src/main/java/org/fostash/atomic/dsl/interfaces/IFilterChain.java
+++ src/main/java/org/fostash/atomic/dsl/interfaces/IFilterChain.java
@@ -1,6 +1,6 @@
 package org.fostash.atomic.dsl.interfaces;
 
-public interface IFilterChain extends IOrderable {
+public interface IFilterChain extends IOrderable, IGroupable {
     IFilterChain and(final ICondition condition);
     IFilterChain or(final ICondition condition);
 }
--- src/main/java/org/fostash/atomic/dsl/interfaces/IOrderable.java
+++ src/main/java/org/fostash/atomic/dsl/interfaces/IOrderable.java
@@ -1,6 +1,7 @@
 package org.fostash.atomic.dsl.interfaces;
 
 public interface IOrderable {
-    IOrderable asc(final IExpression expression);
-    IOrderable desc(final IExpression expression);
+    /*IOrderable asc(final IExpression expression);
+    IOrderable desc(final IExpression expression);*/
+    IFilterChain orderBy(final IExpression... expressions);
 }
