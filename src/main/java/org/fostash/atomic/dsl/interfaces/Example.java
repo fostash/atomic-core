b909c28176b3f7f5f4100198136bac8e1a5856c8
Stefano Altavilla <stefano.altavilla@gmail.com>
2018-08-13 12:40:23 +0200

    [AC-1] Proposed solutions

--- src/main/java/org/fostash/atomic/dsl/interfaces/Example.java
+++ src/main/java/org/fostash/atomic/dsl/interfaces/Example.java
@@ -1,5 +1,9 @@
 package org.fostash.atomic.dsl.interfaces;
 
+import static org.fostash.atomic.dsl.interfaces.IOrderable.SortDirection.ASCENDING;
+import static org.fostash.atomic.dsl.interfaces.IOrderable.SortDirection.DESCENDING;
+import static org.fostash.atomic.dsl.interfaces.IOrderable.sort;
+
 public class Example {
     public Example() {
         final IQuery q = null;
@@ -9,7 +13,11 @@ public class Example {
                 .crossJoin(null)
                 .where(null).and(null).and(null).or(null)
                 .groupBy((IExpression[]) null)
-                .orderBy((IExpression[]) null);
+                .orderBy((IExpression) null)                // Solution #1
+                .orderBy((IExpression) null, DESCENDING)    // Solution #1
+                .orderBy((IExpression) null, ASCENDING)     // Solution #1
+                .orderBy(sort((IExpression) null), sort((IExpression) null, ASCENDING), sort((IExpression) null, DESCENDING)); // Solution #2
+                //.orderBy((IExpression[]) null);
                 //.asc(null).desc(null).desc(null);
     }
 }
--- src/main/java/org/fostash/atomic/dsl/interfaces/IOrderable.java
+++ src/main/java/org/fostash/atomic/dsl/interfaces/IOrderable.java
@@ -1,7 +1,56 @@
 package org.fostash.atomic.dsl.interfaces;
 
+import java.util.Map;
+
 public interface IOrderable {
     /*IOrderable asc(final IExpression expression);
     IOrderable desc(final IExpression expression);*/
-    IFilterChain orderBy(final IExpression... expressions);
+    //IFilterChain orderBy(final IExpression... expressions);
+
+    //////////////////////////////////////////////////////
+    //
+    //  SOLUTION #1
+    //
+    IOrderable orderBy(final IExpression expression, final SortDirection direction);
+
+    default IOrderable orderBy(final IExpression expression) {
+        return this.orderBy(expression, SortDirection.ASCENDING);
+    }
+    //////////////////////////////////////////////////////
+
+
+    //////////////////////////////////////////////////////
+    //
+    //  SOLUTION #2
+    //
+    void orderBy(final Map.Entry<IExpression, SortDirection> ... expressions);
+
+    static Map.Entry<IExpression, SortDirection> sort(final IExpression expression) {
+        return sort(expression, SortDirection.ASCENDING);
+    }
+
+    static Map.Entry<IExpression, SortDirection> sort(final IExpression expression, final SortDirection direction) {
+        return new Map.Entry<IExpression, SortDirection>() {
+            @Override
+            public IExpression getKey() {
+                return expression;
+            }
+
+            @Override
+            public SortDirection getValue() {
+                return direction;
+            }
+
+            @Override
+            public SortDirection setValue(SortDirection value) {
+                throw new UnsupportedOperationException("Changing this value is not supported.");
+            }
+        };
+    }
+    //////////////////////////////////////////////////////
+
+
+    enum SortDirection {
+        ASCENDING, DESCENDING
+    }
 }
